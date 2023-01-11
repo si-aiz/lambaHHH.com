31ff26c950c4b6cdbf98dfaf9f3e054e5032213d
si-aiz <122490110+si-aiz@users.noreply.github.com>
2023-01-11 23:26:51 +0100

    Add files via upload

---
+++ aziz.js
@@ -0,0 +1,10 @@
+function onof(){
+    if(document.getElementById('image').src.match('tafya.jpg')){
+        document.getElementById('image').src="che3la.jpg";
+        document.getElementById('felsa').innerHTML="close it"
+    }
+    else{
+        document.getElementById('image').src="tafya.jpg";
+        document.getElementById('felsa').innerHTML="open it"
+    }
+}
